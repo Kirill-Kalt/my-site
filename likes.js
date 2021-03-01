5eb21764a6c029e400c814662f3bb5d8bdef5c5e
Kirill-Kalt <79315307+Kirill-Kalt@users.noreply.github.com>
2021-03-01 22:26:18 +0600

    Add files via upload

---
+++ likes.js
@@ -0,0 +1,19 @@
+let heart = document.querySelector('.heart');
+let likesNumber = document.querySelector('.likes-number');
+let counter = 0;
+
+heart.onclick = function(){
+  counter++
+  likesNumber.textContent = counter;
+  heart.classList.toggle('added');
+};
+
+
+/*
+if (heart.classList.contains('added')){
+    likesNumber.textContent--;
+}
+else{
+    likesNumber.textContent++;
+}
+heart.classList.toggle('added');*/
